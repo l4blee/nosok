7d7d4155b75848e25c32b4e7c9786295993d6b69
l4blee <76159455+l4blee@users.noreply.github.com>
2021-01-01 21:38:15 +0700

    1

--- commands/music.py
+++ commands/music.py
@@ -40,7 +40,7 @@ class SongQueue:
 
         self.now_playing += 1
         if self.now_playing >= len(self.__queue):
-            self.now_playing = 0
+            self.now_playing = -1
             if not self.repeat:
                 return str()
 
