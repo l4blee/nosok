b4e79991116aba89cbf16c5549909cf58e6fbb41
Kirill Kolobov <76159455+l4blee@users.noreply.github.com>
2021-09-18 19:30:32 +0700

    Update base.py

--- base.py
+++ base.py
@@ -5,7 +5,7 @@ from sqlalchemy import MetaData, create_engine
 from sqlalchemy.ext.declarative import as_declarative
 
 load_dotenv('.env')
-engine = create_engine(os.environ.get('POSTGRES_URL'))
+engine = create_engine(os.environ.get('DATABASE_URL'))
 BASE_PREFIX = os.environ.get('BASE_PREFIX')
 
 
