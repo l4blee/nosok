ad44c2ff3649d25ebdf5b89211c7c5b16ac6aa9f
l4blee <76159455+l4blee@users.noreply.github.com>
2021-10-28 13:57:59 +0700

    upd

--- pinger/index.py
+++ pinger/index.py
@@ -3,5 +3,11 @@ import time
 import os
 
 while True:
-    requests.get(f"http://localhost:{os.environ.get('PORT', 5000)}")
+    print('Trying to connect')
+    try:
+        requests.get(f"http://0.0.0.0:{os.environ.get('PORT', 5000)}",
+                     headers={})
+    except requests.exceptions.ConnectionError:
+        print('connection refused')
+    print('Now sleeping')
     time.sleep(60)
