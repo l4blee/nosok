0f1ef502c02d935f49f15045076097cec727f017
l4blee <76159455+l4blee@users.noreply.github.com>
2021-11-08 17:57:47 +0700

    upd

--- bot/main.py
+++ bot/main.py
@@ -9,7 +9,7 @@ from urllib.parse import urlparse
 from dotenv import load_dotenv
 
 load_dotenv('bot/.env')
-PYTHON_PATH = os.environ.get('PYTHONPATH')
+PYTHON_PATH = os.environ.get('_')
 
 
 class RequestHandler(server.BaseHTTPRequestHandler):
