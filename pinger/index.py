b16c01cf9a5fd241c402f37c1eb27a2876a2f5b8
Kirill Kolobov <76159455+l4blee@users.noreply.github.com>
2021-11-28 16:03:07 +0700

    Update index.py

--- pinger/index.py
+++ pinger/index.py
@@ -1,9 +1,13 @@
 import requests
 import time
+import os
+
+url = os.environ.get('app_url')
 
 while True:
     try:
-        res = requests.get('https://nosok.herokuapp.com')
+        print(f'accessing {url}')
+        res = requests.get(url)
     except requests.exceptions.RequestException:
         print('Request denied...')
     finally:
