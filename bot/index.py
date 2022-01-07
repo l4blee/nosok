93e81b52ccd803c3db27fb2e5b430e95e62da2d8
Kirill Kolobov <76159455+l4blee@users.noreply.github.com>
2022-01-07 16:05:05 +0700

    Update index.py

--- bot/index.py
+++ bot/index.py
@@ -12,7 +12,7 @@ os.makedirs('bot/queues', exist_ok=True)
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(name)s:\t%(message)s',
-                    datefmt='%y.%b.%Y %H:%M:%S')
+                    datefmt='%d.%b.%Y %H:%M:%S')
 
 data_processor.start()
 bot.run()
