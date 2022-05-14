faea85dc48b737197e68590ff6c4c77223d44a1b
l4blee <76159455+l4blee@users.noreply.github.com>
2022-05-14 17:06:27 +0700

    upd

--- bot/cogs/settings.py
+++ bot/cogs/settings.py
@@ -11,7 +11,7 @@ class Settings(commands.Cog):
     @commands.command(aliases=['sp'])
     async def set_prefix(self, ctx: commands.Context, new_prefix: str):
         """
-        Sets prefix for the current server.
+        Sets prefix for the current guild.
         """
         db.guilds.configs.update_one(
             {'guild_id': ctx.guild.id},
@@ -25,12 +25,15 @@ class Settings(commands.Cog):
 
         await send_embed(
             ctx=ctx,
-            description=get_phrase(ctx, 'prefix_set') % dict(new_prefix=new_prefix),
+            description=await get_phrase(ctx, 'prefix_set') % dict(new_prefix=new_prefix),
             color=BASE_COLOR
         )
 
     @commands.command(aliases=['lang', 'setl', 'setloc', 'set_loc'])
     async def set_language(self, ctx: commands.Context, new_lang: str):
+        """
+        Sets language for the current guild.
+        """
         if new_lang not in LOCALES:
             raise exceptions.WrongLocale
             return
@@ -47,7 +50,7 @@ class Settings(commands.Cog):
 
         await send_embed(
             ctx=ctx,
-            description=get_phrase(ctx, 'language_set') % dict(new_lang=new_lang),
+            description=await get_phrase(ctx, 'language_set') % dict(new_lang=new_lang),
             color=BASE_COLOR
         )
 
