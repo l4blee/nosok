c4eadb35a3826c7a2ec4c87c02c6ddbfb4ca99a0
Kirill <l4blee@yandex.ru>
2022-05-31 11:58:13 +0700

    hotfix

---
+++ server/nosok-react/build/precache-manifest.c578d24f289359099572bf3c0c073d45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "38e325da7878cb8ab788",
+    "url": "/static/css/main.776226a7.chunk.css"
+  },
+  {
+    "revision": "38e325da7878cb8ab788",
+    "url": "/static/js/main.38e325da.chunk.js"
+  },
+  {
+    "revision": "3db32729c31753b40041",
+    "url": "/static/js/1.3db32729.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1aba9f8570ffc56255eb800356655a6e",
+    "url": "/static/media/logo.1aba9f85.png"
+  },
+  {
+    "revision": "6456c566001407c299c8730a00430911",
+    "url": "/index.html"
+  }
+];
