546dee57d54f263fba564242c78e094152e31d8d
l4blee <l4blee@yandex.ru>
2022-08-26 10:00:19 +0700

    hotfix

--- frontend/src/runtimeData.tsx
+++ frontend/src/runtimeData.tsx
@@ -2,7 +2,15 @@ import { createStore } from "solid-js/store"
 
 import { io } from 'socket.io-client'
 
-import type { RuntimeStatsStorage, PollingPayload, VarsPayload, APIPayload } from "./interfaces"
+type VarsBlock = {cpu_used: number, memory_used: number}
+type VarsPayload = {status: string, servers: number, vars: VarsBlock}
+
+type RuntimeData = {status: string, servers: number, vars: VarsBlock[]}
+type RuntimeStatsStorage = {log: string, overview: RuntimeData}
+
+type PollingPayload = {changed: string, content: string | VarsPayload}
+type APIPayload = {message: string, content: string | VarsPayload}
+
 
 const socket = io('')
 const [data, setData] = createStore<RuntimeStatsStorage>({
